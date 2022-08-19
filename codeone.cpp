d8b376fd8cb730270e5c33dc5c56108a555d6c45
InshaSaghir <111554927+InshaSaghir@users.noreply.github.com>
2022-08-19 10:01:20 +0500

    Create codeone.cpp

---
+++ codeone.cpp
@@ -0,0 +1,57 @@
+#include<iostrem>
+using namespace std;
+void Add;
+void Sbtract;
+void Mul;
+int main()
+{
+	int choice;
+	cout<<"Please enter your choice\n1:Add\n2:subtract\n3:Mul\n";
+	cin>>choice;
+	switch()
+	case 1:
+		Add();
+		break;
+		case 2:
+			Subtract();
+			break;
+			case 3:
+				Mul();
+				break;
+				default:
+					cout<<"invalid choice";
+	
+}
+void 
+Add()
+{
+	int vauleOne;
+	int vauleTwo;
+	cout<<"Please enter vaule one";
+	cin>>vaule one;
+	cout<<"Please enter vaule two";
+	cin>>vauletwo;
+	cout<<"vauleone+vauletwo";
+}
+
+Add()
+{
+	int vauleOne;
+	int vauleTwo;
+	cout<<"Please enter vaule one";
+	cin>>vaule one;
+	cout<<"Please enter vaule two";
+	cin>>vauletwo;
+	cout<<"vauleone+vauletwo";
+}
+
+Add()
+{
+	int vauleOne;
+	int vauleTwo;
+	cout<<"Please enter vaule one";
+	cin>>vaule one;
+	cout<<"Please enter vaule two";
+	cin>>vauletwo;
+	cout<<"vauleone+vauletwo";
+}
