e8a56d4b9c65d1666137036d7a20e378cdf1d8aa
InshaSaghir <111554927+InshaSaghir@users.noreply.github.com>
2022-08-19 10:08:46 +0500

    Create codetwo.cpp

---
+++ codetwo.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+void  add();
+int main()
+{
+	add();
+	
+	
+}
+void add()
+{
+
+int vaule1, vaule2;
+cout<<"please enter  the number";
+
+cin>>vaule1;
+cout<<"pleaase enter the number";
+cin>>vaule2;
+cout<<vaule1+vaule2;
+}
