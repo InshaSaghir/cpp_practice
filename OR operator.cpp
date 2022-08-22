e16035f2b77028175e10fdd3196420faa9237155
InshaSaghir <111554927+InshaSaghir@users.noreply.github.com>
2022-08-22 18:34:30 +0500

    Update and rename codeone.cpp to OR operator.cpp

---
+++ OR operator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=9;
+	int b=6;
+	if(a==9 || b==8)
+	{
+		cout<<"the condition is right";
+	}
+	else
+	{
+		cout<<"the condition is wrong";
+	}
+}
