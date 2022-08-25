deaa6b0f9d809336a02fbceeb488d5c4892edc99
InshaSaghir <111554927+InshaSaghir@users.noreply.github.com>
2022-08-25 07:27:56 +0500

    Create equal operator.cpp

---
+++ equal operator.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int vaule1;
+	int vaule2;
+	cout<<"Please enter the number"<<endl;
+	cin>>vaule1;
+	cout<<"Please enter the second number"<<endl;
+	cin>>vaule2;
+	if(vaule1==vaule2)
+	{
+		cout<<"the condtion is right"<<endl;
+	}
+	else
+	{
+		cout<<"The condition is wrong";
+	}
+	
+	}
